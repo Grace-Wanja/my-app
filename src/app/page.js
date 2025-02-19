0d9e9b458a6ee6d3701a17729e9210ff742afa39
Grace-Wanja <gwanja256@gmail.com>
2025-02-19 11:13:49 +0300

    initial change

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,24 @@
 export default function Home() {
-  let 
-  return 1 + 99;
+
+  let first_number = 923;
+  let second_number = 7
+  let  sum = first_number + second_number;
+
+let username = "Okello Lutwa";
+
+  return (
+    <div>
+      <h1>DATA SCIENCE FOR AFRICA</h1>
+      <h2>An initiative for 2025</h2>
+      <p> Welcome to our website</p>
+
+      <strong>{10 + 10 * 1000}</strong>
+      <br/>
+      <em>{sum}</em>
+
+      <p>Greetings, {username} 👋</p>
+      <p>Greetings, {username} 👋</p>
+      
+    </div>
+  )
 }
